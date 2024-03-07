56022e682370f4e851c8824961b189e4a3d2c11a
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-07 09:40:52 -0300

    :sparkles: feat: Inserindo responsividade nos cards

--- src/components/CardGridLayout/index.tsx
+++ src/components/CardGridLayout/index.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from "react";
 import { parserGridArea } from "../../utils/parser";
+import "./style.scss"
+import { useMediaQuery } from "usehooks-ts";
+import variables from '../../styles/variables.module.scss';
 
 type GridLayoutProps = {
  children:ReactNode,
@@ -7,8 +10,11 @@ type GridLayoutProps = {
 }
  
 const GridLayout =({ gridAreas, children  }: GridLayoutProps) =>{
+    const mobileMatch = useMediaQuery(`(max-width:${variables.mobile})`)
+    const tabletMatch = useMediaQuery(`(min-width:calc(${variables.mobile} + 1px)) and (max-width:${variables.tablet})`)
+    console.log("mobileMatch",mobileMatch,tabletMatch)
     return (
-        <div style={{display:"grid", gridTemplateAreas:parserGridArea(gridAreas) , width:"100%"}}>
+        <div style={{display:"grid", gridTemplateAreas:parserGridArea(gridAreas) }} className="cards--grid">
             {children}
         </div>
     );
--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -2,7 +2,6 @@ export const parserGridArea = (gridsArray:string[]):string =>
 {
     return gridsArray.reduce((acc, current)=> 
         {
-            console.log("acc",acc, current)
             return (acc = acc + `${"'" + current + "'"}`)
         }, "")
 }
